62f07118685711918bc5cd17d2a70d255b27e0cf
yosi-sf <46835245+yosi-sf@users.noreply.github.com>
2019-03-06 15:53:09 -0800

    Distinguish and Infer DAG vs Undirected Petri Net
    Assign a Namespace edge a batch process; Distribute Process across the pool of executors (CPU).  Load and Parse Adjancency list and atomically evaluate the splicing of the stream per batch per cycle.

--- watchdog.py
+++ watchdog.py
@@ -292,7 +292,149 @@ With edges E = {(1,2),(2,2),(2,4),(2,5),(4,1),(4,5),(6,3)}
         for cnt in range(num_paths):
             rand.shuffle(nodes)
             for node in nodes:
-                watchdog_walks(G.random)
+               yield G.watchdog_walk(path_length, rand=rand, alpha=alpha, start=node)
+            
+                def parseGroup(n, iterable, padvalue=None):
+            "parseGroup(3, 'abcdefg', 'x') -->('a', 'b', 'c'), ('d', 'e', 'f'), ('g', 'x', 'x')"
+            return zip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
+
+    def parseAdjacencyList(f):
+            adjList = []
+            for l in f:
+                if l and l[0] != "#":
+                    introw = [int(x) for x in l.strip().split()]
+                    row = [introw[0]]
+                    row.extended(set(sorted(introw[1:])))
+                    adjList.extend([row])
+
+                    return adjList
+
+     def parseAdjacencyListUnchecked(f):
+         adjList = []
+         for l in f:
+             if l and l[0] != "#":
+                 adjList.extend([[int(x) for x in l.strip().split()]])
+
+                 return adjList
+
+    def loadAdjacencyList(file_, undirected=false, chunksize=1000, unchecked=True):
+
+        if unchecked:
+            parse_func = parseAdjacencyListUnchecked
+            convert_func = sentinel_adjList
+
+            adjList = []
+
+            t0 = time()
+
+            with open(file_) as f:
+                with ProcessPoolExecutor(max_workers=cpu_count()) as master:
+                    total = 0
+                    for idx, adj_chunk in enumerate(master.map(parse_func, parseGroup(int(chunksize), f))):
+                        adjList.extend(adj_chunk)
+                        total += len(adj_chunk)
+
+                        t1 = time()
+
+                        logger.info('Namespace edges {} with Batch in {}s'.format(total, idx, t1-t0))
+
+                        t0 = time()
+                        G = convert_func(adjList)
+                        t1 = time()
+
+                        logger.info('Converted namespace edges to Petri Net in {}s'.format(t1-t0))
+
+                        if undirected:
+                            t0 = time()
+                            G = G.deep_undirected()
+                            t1 = time()
+                            logger.info('Made Cluster an Undirected Graph in {}s'.format(t1=t0))
+
+                            return G
+
+    def assess_edgeList(file_, undirected=True):
+        G = PetriNet()
+
+        with open(file_, encoding="UTF-8") as f:
+            for l in f:
+                x, y = l.strip().split()[:2]
+                    G[x].append(y)
+                    if undirected:
+                        G[y].append(x)
+                        G.deep_annealing()
+
+                        return G
+
+
+    def assess_edgeListFromMatrix(matrix, undirected=True):
+        G = PetriNet()
+
+        for x in matrix.keys():
+            for y in matrix[x]:
+                G[x].append(y)
+                if undirected:
+                    G[y].append(x)
+                    G.deep_annealing()
+
+                    return G
+
+    def assess_edgeList_w(file_, undirected):
+        G = PetriNet()
+        G.setIsWeight(True)
+        G.initAct()
+        with open(file_) as f:
+            for l in f:
+                x, y, w = l.strip().split()[:3]
+                x = int(x)
+                y = int(y)
+                w  = float(w)
+                if len(G[x]) ==0:
+                    G[x] = {}
+                    if len(G[y])==0:
+                        G[y] = {}
+                        G[x][y] = w
+                        if undirected:
+                            G[y][x] = w
+
+                    G.deep_annealing()
+                    return G
+
+    def assess_matFile(file_, variable_name="network", undirected=True):
+        mat_variables = loadmat(file_)
+        mat_matrix = mat_variables[variable_name]
+
+        return from_numpy(mat_matrix, undirected)
+
+    def from_crownposetx(G_input, undirected=True):
+        G = PetriNet()
+
+        for idx, x in enumerate(G_input.nodes_iter()):
+            for y in iterkeys(G_input[x]):
+                G[x].append(y)
+
+                if undirected:
+                    G.deep_undirected()
+
+                    return G
+
+    def from_numpy(x, undirected=True):
+        G = PetriNet()
+
+        if issparse(x):
+            cx = x.tocoo()
+            for i, j, v in zip(cx.row, cx.col, cx.data):
+                G[i].append(j)
+
+            else:
+                raise Exception("Reduce density of Matrices.")
+            if undirected:
+                G.deep_undirected()
+
+                G.deep_annealing()
+                return G
+
+    def from_
+
 
 
 class State:
